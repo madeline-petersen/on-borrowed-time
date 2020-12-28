ffb983e9bb07c37fb468b364b5428953d35390b4
Madeline Petersen <madeline.petersen@ibm.com>
2020-12-28 13:59:43 -0800

    Add index page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
 import './App.css';
 
+import {
+  Link,
+  Route,
+  BrowserRouter as Router,
+  Switch
+} from "react-router-dom";
+
 import { ScreenClassProvider } from 'react-grid-system';
-import logo from './logo.svg';
 
 function App() {
   return (
+    <Router>
     <ScreenClassProvider>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/index">Index</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <div>Home</div>
+          </Route>
+          <Route path="/index">
+            <div>Index</div>
+          </Route>
+        </Switch>
+      </div>
     </ScreenClassProvider>
+    </Router>
   );
 }
 
