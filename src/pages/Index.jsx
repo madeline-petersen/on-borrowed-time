6fac9854e031cd25b6f80cb93ea7fc6972b3d72c
Madeline Petersen <madeline.petersen@ibm.com>
2020-12-28 16:18:50 -0800

    Use column offset

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -6,8 +6,7 @@ const Index = () => {
   return (
     <Container debug className="container-1">
         <Row debug className="col">
-            <Col debug md={4} className="col"></Col>
-            <Col debug md={8} className="col">All<br />Language<br />Censorship<br />Collective Memory<br />Diaspora<br />In-Betweeness<br />National Identity</Col>
+            <Col offset={{ md: 4 }} debug md={8} className="col">All<br />Language<br />Censorship<br />Collective Memory<br />Diaspora<br />In-Betweeness<br />National Identity</Col>
         </Row>
     </Container>
   );
