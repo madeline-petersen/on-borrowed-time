6f7afcf5d06874c83727193d4cd78d9f1d6452b4
Madeline Petersen <madeline.petersen@ibm.com>
2020-12-28 22:14:38 -0800

    Use offset

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -6,15 +6,11 @@ const Index = () => {
   return (
     <div className={`Index index-helper isVisible`}>
     <Container className="grid-container">
-      <Row className="bx--row">
-        <Col md={1}/>
-        <Col md={1}/>
-        <Col md={1}/>
-        <Col md={1}/>
-        <Col md={8}>All<br />Language<br />Censorship<br />Collective Memory<br />Diaspora<br />In-Betweeness<br />National Identity</Col>
+    <Row className="bx--row">
+        <Col offset={{md: 4}} md={8}>All<br />Language<br />Censorship<br />Collective Memory<br />Diaspora<br />In-Betweeness<br />National Identity</Col>
       </Row>
     </Container>
-  </div>  )
+  </div>);
 }
 
 export default Index;
