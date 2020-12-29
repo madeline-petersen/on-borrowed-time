57a71aed92f40ee4570242ea7848aec527c574d1
Madeline Petersen <madeline.petersen@ibm.com>
2020-12-28 23:51:13 -0800

    Fill out index page

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,6 +3,29 @@ import './Index.css';
 import { Col, Container, Row } from 'react-grid-system';
 
 const Index = () => {
+
+  const entry = (
+    <>
+    <Row className="bx--row index-entry">
+      <Col md={4} className="regular-caption">Death of an Icon</Col>
+      <Col md={3} className="regular-caption">Champion for Liberalisation</Col>
+      <Col md={3} className="regular-caption light">China’s Struggle for Democracy, Its…</Col>
+      <Col md={2} className="regular-caption light">Excerpt (Book) 1989</Col>
+    </Row>
+    <Row className="bx--row index-entry">
+      <Col md={4} className="regular-caption"></Col>
+      <Col md={3} className="regular-caption">Death of Hu Yaobang</Col>
+      <Col md={3} className="regular-caption light">Tiananmen Papers</Col>
+      <Col md={2} className="regular-caption light">Excerpt (Book) 1989</Col>
+    </Row>
+    <Row className="bx--row index-entry">
+      <Col md={4} className="regular-caption"></Col>
+      <Col md={3} className="regular-caption">Mourning of Hu Yaobang</Col>
+      <Col md={3} className="regular-caption light">Tiananmen Papers</Col>
+      <Col md={2} className="regular-caption light">Excerpt (Book) 1989</Col>
+    </Row>
+    </>
+  )
   return (
     <div className="Index index-helper">
     <Container className="grid-container">
@@ -13,10 +36,19 @@ const Index = () => {
       <Row className="bx--row lower-heading">
         <Col md={4} className="medium-caption">Event</Col>
         <Col md={3} className="medium-caption">Anecdotes</Col>
-        <Col md={4} className="medium-caption">Source</Col>
-        <Col md={1} className="medium-caption">Type</Col>
+        <Col md={3} className="medium-caption">Source</Col>
+        <Col md={2} className="medium-caption">Type</Col>
       </Row>
       <Row className="bx--row divider thin" />
+      <Row className="bx--row event-header">
+        <Col md={4} className="medium-body">The June Fourth “Incident”<br />1989</Col>
+      </Row>
+      <Row className="bx--row divider thinnest" />
+      {entry}
+      <Row className="bx--row divider thinnest" />
+      {entry}
+      <Row className="bx--row divider thinnest" />
+      {entry}
     </Container>
   </div>);
 }
