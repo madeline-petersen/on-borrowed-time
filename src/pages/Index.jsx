d2447f2d14965a2139e256a9b3ab1195a841d95f
Madeline Petersen <madeline.petersen@ibm.com>
2020-12-28 17:01:39 -0800

    Add font

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -4,9 +4,9 @@ import { Col, Container, Row } from 'react-grid-system';
 
 const Index = () => {
   return (
-    <Container debug className="container-1">
-        <Row debug className="col">
-            <Col offset={{ md: 4 }} debug md={8} className="col">All<br />Language<br />Censorship<br />Collective Memory<br />Diaspora<br />In-Betweeness<br />National Identity</Col>
+    <Container className="container-1 Index">
+        <Row className="col">
+            <Col offset={{ md: 4 }} md={8} className="col">All<br />Language<br />Censorship<br />Collective Memory<br />Diaspora<br />In-Betweeness<br />National Identity</Col>
         </Row>
     </Container>
   );
