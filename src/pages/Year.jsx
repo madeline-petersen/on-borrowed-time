99a269e9f31a64641a45ddebaa42e255e75cea65
Madeline Petersen <madeline.petersen@ibm.com>
2021-05-14 23:51:22 +0000

    Add home page

--- src/pages/Year.jsx
+++ src/pages/Year.jsx
@@ -13,35 +13,18 @@ import React from 'react';
 import Reflection from './Reflection.jsx';
 import data from '../data/years.json';
 
-// const Year = () => {
-//   const year = data.years[0]; // number, title
-//   const scene = data.years[0].scenes[0]; // title, event, imagery, reflection
-//   const event = data.years[0].scenes[0].event; // paragraphs, resources
-//   let match = useRouteMatch();
-//   console.log('match ', match);
-
-//   return (
-//     <div className="Year">
-//       <Event year={year} event={event} scene={scene} />
-//     </div>
-//   );
-// };
-
 function Years() {
   let match = useRouteMatch();
 
   return (
     // The Years page has its own <Switch> with more routes
-    // that build on the /topics URL path. You can think of the
-    // 2nd <Route> here as an "index" page for all topics, or
-    // the page that is shown when no topic is selected
-
+    // that build on the /{year} URL path. You can think of the
     <Switch>
       <Route path={`${match.path}/:topicId`}>
         <Page />
       </Route>
       <Route path={match.path}>
-        <h3>Please select a topic.</h3>
+        <h3>Home</h3>
       </Route>
     </Switch>
   );
