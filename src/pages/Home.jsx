32f79ecf0f1d0ad68ed5ce764aaaeb195cdb10e1
Madeline Petersen <madeline.petersen@ibm.com>
2021-05-18 00:52:27 +0000

    Viewport hack for mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import './Home.scss';
+
 import { Col, Container, Row } from 'react-grid-system';
 
 import React from 'react';
@@ -5,6 +7,17 @@ import { useScreenClass } from 'react-grid-system';
 
 const Home = () => {
   const screenClass = useScreenClass();
+
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  // We listen to the resize event
+  window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
 
   // Set the date we're counting down to
   let countDownDate = new Date('Jul 1, 2047 00:00:00').getTime();
@@ -47,56 +60,54 @@ const Home = () => {
   }, 1000);
 
   return (
-    <div className="absolute inset-0">
-      <div className="h-screen bg-black">
-        <Container className="grid__container">
-          <Row
-            className="grid__row"
-            style={{
-              margin: ['lg', 'xl', 'xxl'].includes(screenClass)
-                ? '0 -74px' // lg, xl, xxl
-                : ['md'].includes(screenClass)
-                ? '0 -64px' // md
-                : '0 -26px' // sm, xs
-            }}
-          >
-            <div className="h-screen w-full flex flex-col justify-between py-5">
-              {/* top container */}
-              <div>
-                <Col lg={12} md={12} sm={12} xs={12}>
-                  <div className="flex justify-between items-center mb-2.5">
-                    <h3 className="small-headline text-white">
-                      A Borrowed Place, On Borrowed Time
-                    </h3>
-                    {/* hide below md breakpoint */}
-                    <h3 className="small-headline-characters text-white sm:block hidden">
-                      暫借的地方，暫借的時間
-                    </h3>
-                  </div>
-                </Col>
-                <Col lg={12} md={12} sm={12} xs={12}>
-                  {/* show below md breakpoint */}
-                  <h3 className="small-headline-characters text-white sm:hidden block pt-1 mb-5">
+    <div className="my-element bg-black">
+      <Container className="grid__container">
+        <Row
+          className="grid__row"
+          style={{
+            margin: ['lg', 'xl', 'xxl'].includes(screenClass)
+              ? '0 -74px' // lg, xl, xxl
+              : ['md'].includes(screenClass)
+              ? '0 -64px' // md
+              : '0 -26px' // sm, xs
+          }}
+        >
+          <div className="my-element w-full flex flex-col justify-between py-5">
+            {/* top container */}
+            <div>
+              <Col lg={12} md={12} sm={12} xs={12}>
+                <div className="flex justify-between items-center mb-2.5">
+                  <h3 className="small-headline text-white">
+                    A Borrowed Place, On Borrowed Time
+                  </h3>
+                  {/* hide below md breakpoint */}
+                  <h3 className="small-headline-characters text-white sm:block hidden">
                     暫借的地方，暫借的時間
                   </h3>
-                </Col>
-                <Col lg={12} md={12} sm={12} xs={12}>
-                  <h3 className="small-body text-white">Project coming soon</h3>
-                </Col>
-              </div>
-
-              {/* bottom container */}
-              <div>
-                <Col lg={12} md={12} sm={12} xs={12}>
-                  <h3 className="small-body text-white opacity-50">
-                    The Handover — <span id="demo" />
-                  </h3>
-                </Col>
-              </div>
+                </div>
+              </Col>
+              <Col lg={12} md={12} sm={12} xs={12}>
+                {/* show below md breakpoint */}
+                <h3 className="small-headline-characters text-white sm:hidden block pt-1 mb-5">
+                  暫借的地方，暫借的時間
+                </h3>
+              </Col>
+              <Col lg={12} md={12} sm={12} xs={12}>
+                <h3 className="small-body text-white">Project coming soon</h3>
+              </Col>
             </div>
-          </Row>
-        </Container>
-      </div>
+
+            {/* bottom container */}
+            <div>
+              <Col lg={12} md={12} sm={12} xs={12}>
+                <h3 className="small-body text-white opacity-50">
+                  The Handover — <span id="demo" />
+                </h3>
+              </Col>
+            </div>
+          </div>
+        </Row>
+      </Container>
     </div>
   );
 };
