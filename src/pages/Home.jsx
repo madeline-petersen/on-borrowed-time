70c10abd4b3f5fef59f7a1aade534bbf4ccc39a1
Madeline Petersen <madeline.petersen@ibm.com>
2021-05-19 02:41:00 +0000

    Add image to splash page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import './Home.scss';
 import { Col, Container, Row } from 'react-grid-system';
 
 import React from 'react';
+import onborrowedtime from '../images/onborrowedtime.jpg';
 import { useScreenClass } from 'react-grid-system';
 
 const Home = () => {
@@ -61,34 +62,48 @@ const Home = () => {
 
   return (
     <div className="screen-height bg-black w-full flex flex-col justify-between p-4 md:p-5">
-      {/* top container */}
       <div>
-        <div className="flex justify-between items-center lg:mb-2 md:mb-1">
-          <h3
-            className={`hidden md:block small-headline text-white text-fade-in`}
-          >
-            A Borrowed Place, On Borrowed Time
-          </h3>
-          <h3
-            className={`block md:hidden mobile-headline text-white text-fade-in mb-1`}
-          >
-            A Borrowed Place, On Borrowed Time
-          </h3>
-
-          {/* hide below md breakpoint */}
-          <h3 className="headline-characters text-white md:block hidden text-fade-in">
-            暫借的地方, 暫借的時間
-          </h3>
+        <div className="flex md:hidden w-100 justify-center">
+          <div className="mobile-container">
+            <img src={onborrowedtime} alt="" className="text-fade-in" />
+          </div>
         </div>
 
-        {/* show below md breakpoint */}
-        <h3 className="mobile-characters text-white md:hidden mb-3.5 pb-px block text-fade-in">
-          暫借的地方, 暫借的時間
-        </h3>
+        {/* top container */}
+        <div>
+          <div className="flex justify-between items-center lg:mb-2 md:mb-1">
+            <h3
+              className={`hidden md:block small-headline text-white text-fade-in`}
+            >
+              A Borrowed Place, On Borrowed Time
+            </h3>
+            <h3
+              className={`block md:hidden mobile-headline text-white text-fade-in mt-3.5 pt-px mb-1`}
+            >
+              A Borrowed Place, On Borrowed Time
+            </h3>
 
-        <h3 className="small-body text-gray-60 text-fade-in">
-          Anthology coming soon
-        </h3>
+            {/* hide below md breakpoint */}
+            <h3 className="headline-characters text-white md:block hidden text-fade-in">
+              暫借的地方, 暫借的時間
+            </h3>
+          </div>
+
+          {/* show below md breakpoint */}
+          <h3 className="mobile-characters text-white md:hidden mb-3.5 pb-px block text-fade-in">
+            暫借的地方, 暫借的時間
+          </h3>
+
+          <h3 className="small-body text-gray-60 text-fade-in">
+            Anthology coming soon
+          </h3>
+        </div>
+      </div>
+
+      <div className="md:flex hidden w-100 justify-center">
+        <div className="desktop-container">
+          <img src={onborrowedtime} alt="" className="text-fade-in" />
+        </div>
       </div>
 
       {/* bottom container */}
