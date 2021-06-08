d933ca05a447843fe8b6cdf7c4442c3c5683dd67
Madeline Petersen <madeline.petersen@ibm.com>
2021-06-08 21:55:54 +0000

    Add content to intro pages

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -3,7 +3,10 @@ import './Intro.scss';
 import { Col, Container, Row } from 'react-grid-system';
 import React, { useEffect, useState } from 'react';
 
+import Footer from '../components/Footer';
 import PropTypes from 'prop-types';
+import stairs from '../images/Homepage_1989.jpg';
+import temple from '../images/Homepage_1984.jpg';
 
 const Intro = ({ year }) => {
   const [isClicked, setClicked] = useState(false);
@@ -39,6 +42,46 @@ const Intro = ({ year }) => {
             </Container>
           </div>
         </div>
+        <Container className="grid__container min-h-screen">
+          <Row className="grid__row pt-64 pb-40">
+            <Col lg={4} />
+            <Col lg={7} md={12}>
+              <p className={`large-headline-dynamic text-white pb-16`}>
+                Quisque non sem lacinia, sagittis nulla in, fermentum augue.
+                Aenean in quam id neque commodo dignissim. Cras tincidunt semper
+                metus sed consectetur. Nullam vitae facilisis sapien, a sodales
+                ligula. Morbi id massa et nisl molestie rhoncus non id quam.
+              </p>
+            </Col>
+            <Col lg={1} />
+          </Row>
+
+          <Row className="grid__row pb-20">
+            <Col lg={1} />
+            <Col lg={11} md={12}>
+              <img src={temple} alt="" />
+            </Col>
+          </Row>
+
+          <Row className="grid__row pb-20">
+            <Col lg={1} />
+            <Col lg={11} md={12}>
+              <img src={stairs} alt="" />
+            </Col>
+          </Row>
+
+          <Footer
+            nextParams={{ year: year.id, scene: 'scene-I', page: 'event' }}
+            next={year.scenes[0]}
+            changingParam="scene"
+            setClicked={setClicked}
+            theme={{
+              background: 'black',
+              text: 'gray-40',
+              border: 'gray-80'
+            }}
+          />
+        </Container>
       </div>
     </>
   );
