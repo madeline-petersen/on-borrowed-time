66c184434187adbe4bd8f7ac970b9322de5e3fae
Madeline Petersen <madeline.petersen@ibm.com>
2021-07-20 04:38:57 +0000

    Adjust border/text colours in resource table

--- src/components/ResourceTable.jsx
+++ src/components/ResourceTable.jsx
@@ -35,7 +35,9 @@ const ResourceTable = ({ data }) => {
       <Row className={`grid__row`}>
         <Col lg={3} md={2} />
         <Col lg={9} md={10}>
-          <p className={`border-t border-gray-40 pb-4 fade-in-element`} />
+          <p
+            className={`border-t border-black border-opacity-10 pb-4 fade-in-element`}
+          />
         </Col>
       </Row>
       {data.map((entry, index) => {
@@ -46,7 +48,7 @@ const ResourceTable = ({ data }) => {
                 <Col lg={3} md={2} />
                 <Col lg={9} md={10}>
                   <p
-                    className={`border-t border-gray-40 pt-4 fade-in-element`}
+                    className={`border-t border-black border-opacity-10 pt-4 fade-in-element`}
                   />
                 </Col>
               </>
@@ -58,12 +60,12 @@ const ResourceTable = ({ data }) => {
               className="contents cursor-pointer"
             >
               <Col lg={4} md={4} sm={4} xs={12} className="small-body">
-                <p className={`text-black fade-in-element`}>
+                <p className={`text-black text-opacity-90 fade-in-element`}>
                   {ReactHtmlParser(entry.shortTitle)}
                 </p>
               </Col>
               <Col lg={3} md={3} sm={4} xs={12} className="small-body">
-                <p className={`text-gray-70 fade-in-element`}>
+                <p className={`text-black text-opacity-60 fade-in-element`}>
                   {ReactHtmlParser(
                     `${entry.bookTitle ? entry.bookTitle : entry.publication}`
                   )}
@@ -77,8 +79,12 @@ const ResourceTable = ({ data }) => {
                 className={`small-body`}
                 style={{ display: 'flex', justifyContent: 'space-between' }}
               >
-                <p className="text-gray-70 fade-in-element">{entry.type}</p>
-                <p className="text-gray-70 fade-in-element">{entry.year}</p>
+                <p className="text-black text-opacity-60 fade-in-element">
+                  {entry.type}
+                </p>
+                <p className="text-black text-opacity-60 fade-in-element">
+                  {entry.year}
+                </p>
               </Col>
             </span>
             <Col lg={3} md={2} />
