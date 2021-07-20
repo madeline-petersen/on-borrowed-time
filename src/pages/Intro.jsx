60c030941f3f1806fecccfaaa1400b98c7826005
Madeline Petersen <madeline.petersen@ibm.com>
2021-07-20 03:51:39 +0000

    Remove scale transform

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -68,11 +68,7 @@ const Intro = ({
       </div>
 
       <div className={`intro ${isClicked ? 'fade-out' : 'foreground-fade-in'}`}>
-        <div
-          className={`hero-image ${yearBackgroundClasses[year.id]} ${
-            !isTransitioning ? 'hero-image-scale' : ''
-          }`}
-        >
+        <div className={`hero-image ${yearBackgroundClasses[year.id]}`}>
           <div className="small-headline text-white absolute w-full scene-name">
             <Container className="grid__container">
               <Row className="grid__row">
