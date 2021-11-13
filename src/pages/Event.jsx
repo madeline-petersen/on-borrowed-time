f4b4c766601edeb0ffa88c17f4ae9e2ccce9a012
Madeline Petersen <9222076+maddleline@users.noreply.github.com>
2021-11-12 22:31:25 -0800

    Add images

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -17,6 +17,10 @@ import hkIdentity from '../images/02_2020_HKIdentity.jpeg';
 import geniuneDemocracy from '../images/03_2020_GeniuneDemocracy.jpeg';
 import solidarityAndResistance from '../images/04_2020_SolidarityAndResistance.jpeg';
 import nationalAmnesia from '../images/05_2020_NationalAmnesia.jpeg';
+import one from '../images/1997-01.jpeg';
+import two from '../images/1997-02.jpeg';
+import three from '../images/1997-03.jpeg';
+import four from '../images/1997-04.jpeg';
 
 const imageLookup = {
   hkIdentity: hkIdentity,
@@ -26,7 +30,11 @@ const imageLookup = {
   car: car,
   crowd: crowd,
   tanks: tanks,
-  candles: candles
+  candles: candles,
+  one: one,
+  two: two,
+  three: three,
+  four: four
 };
 
 const Event = ({
