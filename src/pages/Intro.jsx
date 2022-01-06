f8541edfbcd0641c455d320eaea88c30079171eb
Madeline Petersen <9222076+maddleline@users.noreply.github.com>
2022-01-05 19:16:33 -0800

    Add hardscroll to Intro

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,12 +1,12 @@
 import './Intro.scss';
 
 import { Col, Container, Row } from 'react-grid-system';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Footer from '../components/Footer';
 import PropTypes from 'prop-types';
 import ReactHtmlParser from 'react-html-parser';
-import { useOverscroll } from '../hooks/useOverscroll';
+import ReactFullpage from '@fullpage/react-fullpage';
 
 const Intro = ({
   year,
@@ -40,11 +40,11 @@ const Intro = ({
     );
   };
 
-  const onScrollEnd = useCallback(() => {
-    onClickYear();
-  }, []);
-  const scrollRef = useRef();
-  useOverscroll(scrollRef, onScrollEnd, 0);
+  const afterLoad = function(origin, destination, direction) {
+    if (destination.isLast) {
+      onClickYear();
+    }
+  };
 
   return (
     <>
@@ -52,7 +52,7 @@ const Intro = ({
       <div className={`absolute top-0 w-full`}>
         <div
           // fades to black before transitioning
-          className={`bg-black w-full h-95vh ${
+          className={`bg-black w-full h-screen ${
             isClicked ? 'screen-shrink' : ''
           }`}
         />
@@ -63,48 +63,65 @@ const Intro = ({
         )}
       </div>
 
-      <div
-        className={`intro cursor-pointer ${
-          isClicked ? 'fade-out' : 'foreground-fade-in'
-        }`}
-        ref={scrollRef}
-        onClick={() => onClickYear(year.id)}
-      >
-        <div className={`hero-image ${backgroundClass}`}>
-          <div
-            key={`gradient-transition-${year.id}`}
-            className="gradient-transition"
-          >
-            <div className="small-headline text-white absolute w-full blurb-content">
-              <Container className="grid__container">
-                <Row className="grid__row">
-                  <Col lg={5} />
-                  <Col lg={7}>
-                    <p className="fade-first">{ReactHtmlParser(year.blurb)}</p>
-                  </Col>
-                </Row>
-              </Container>
-            </div>
-            <div className="absolute w-full bottom-0">
-              <Container className="grid__container">
-                <Footer
-                  pageId="intro"
-                  nextParams={nextParams}
-                  next={year.scenes[0]}
-                  changingParam="scene"
-                  setClicked={setClicked}
-                  setIsTransitioning={setIsTransitioning}
-                />
-              </Container>
-            </div>
-          </div>
-          <div className={`w-full ${colourBackgroundClass}`}>
-            <Container className="grid__container">
-              <Row className={`grid__row`} style={{ height: '5vh' }}></Row>
-            </Container>
-          </div>
-        </div>
-      </div>
+      <ReactFullpage
+        licenseKey={'518F7C98-E6514A4C-AF78105C-8D322AE9'}
+        scrollingSpeed={1000}
+        afterLoad={afterLoad}
+        render={({ state, fullpageApi }) => {
+          return (
+            <ReactFullpage.Wrapper>
+              <div
+                className={`section intro cursor-pointer ${
+                  isClicked ? 'fade-out' : 'foreground-fade-in'
+                }`}
+                onClick={() => fullpageApi.moveSectionDown()}
+              >
+                <div className={`hero-image ${backgroundClass}`}>
+                  <div
+                    key={`gradient-transition-${year.id}`}
+                    className="gradient-transition"
+                  >
+                    <div className="small-headline text-white absolute w-full blurb-content">
+                      <Container className="grid__container">
+                        <Row className="grid__row">
+                          <Col lg={5} />
+                          <Col lg={7}>
+                            <p className="fade-first">
+                              {ReactHtmlParser(year.blurb)}
+                            </p>
+                          </Col>
+                        </Row>
+                      </Container>
+                    </div>
+                    <div className="absolute w-full bottom-0">
+                      <Container className="grid__container">
+                        <Footer
+                          pageId="intro"
+                          nextParams={nextParams}
+                          next={year.scenes[0]}
+                          changingParam="scene"
+                          setClicked={setClicked}
+                          setIsTransitioning={setIsTransitioning}
+                          navigateTo={navigateTo}
+                          onClick={() => fullpageApi.moveSectionDown()}
+                        />
+                      </Container>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={`section w-full ${colourBackgroundClass}`}>
+                <Container className="grid__container">
+                  <Row
+                    className={`grid__row`}
+                    style={{ height: '100vh' }}
+                  ></Row>
+                </Container>
+              </div>
+            </ReactFullpage.Wrapper>
+          );
+        }}
+      />
     </>
   );
 };
