c2e28118da82d8fd2219e03b3469742d359e47c6
Madeline Petersen <9222076+maddleline@users.noreply.github.com>
2022-01-18 21:33:48 -0800

    Refactor

--- src/pages/ThematicThreads.jsx
+++ src/pages/ThematicThreads.jsx
@@ -14,81 +14,76 @@ import data from '../data/thematic-threads.json';
 const ThematicThreads = ({ currentBgColour }) => {
   let history = useHistory();
 
-  const evenTitleCols = {
-    xl: 11,
-    lg: 11,
-    md: 11,
-    sm: 11
+  const evenCols = {
+    title: {
+      xl: 11,
+      lg: 11,
+      md: 11,
+      sm: 11
+    },
+    titleGutterRight: {
+      xl: 1,
+      lg: 1,
+      md: 1,
+      sm: 1
+    },
+    subtitleGutterLeft: {
+      xl: 1,
+      lg: 1,
+      md: 1,
+      sm: 1
+    },
+    subtitle: {
+      xl: 6,
+      lg: 6,
+      md: 6,
+      sm: 6
+    },
+    subtitleGutterRight: {
+      xl: 5,
+      lg: 5,
+      md: 5,
+      sm: 5
+    }
   };
 
-  const evenTitleGutterRightCols = {
-    xl: 1,
-    lg: 1,
-    md: 1,
-    sm: 1
-  };
-
-  const evenSubtitleGutterLeftCols = {
-    xl: 1,
-    lg: 1,
-    md: 1,
-    sm: 1
-  };
-
-  const evenSubtitleCols = {
-    xl: 6,
-    lg: 6,
-    md: 6,
-    sm: 6
-  };
-
-  const evenSubtitleGutterRightCols = {
-    xl: 5,
-    lg: 5,
-    md: 5,
-    sm: 5
-  };
-
-  const oddTitleGutterLeftCols = {
-    xl: 2,
-    lg: 2,
-    md: 2,
-    sm: 2
-  };
-
-  const oddTitleCols = {
-    xl: 9,
-    lg: 9,
-    md: 9,
-    sm: 9
-  };
-
-  const oddTitleGutterRightCols = {
-    xl: 1,
-    lg: 1,
-    md: 1,
-    sm: 1
-  };
-
-  const oddSubtitleGutterLeftCols = {
-    xl: 3,
-    lg: 3,
-    md: 3,
-    sm: 3
-  };
-
-  const oddSubtitleCols = {
-    xl: 8,
-    lg: 8,
-    md: 8,
-    sm: 8
-  };
-
-  const oddSubtitleGutterRightCols = {
-    xl: 1,
-    lg: 1,
-    md: 1,
-    sm: 1
+  const oddCols = {
+    titleGutterLeft: {
+      xl: 2,
+      lg: 2,
+      md: 2,
+      sm: 2
+    },
+    title: {
+      xl: 9,
+      lg: 9,
+      md: 9,
+      sm: 9
+    },
+    titleGutterRight: {
+      xl: 1,
+      lg: 1,
+      md: 1,
+      sm: 1
+    },
+    subtitleGutterLeft: {
+      xl: 3,
+      lg: 3,
+      md: 3,
+      sm: 3
+    },
+    subtitle: {
+      xl: 8,
+      lg: 8,
+      md: 8,
+      sm: 8
+    },
+    subtitleGutterRight: {
+      xl: 1,
+      lg: 1,
+      md: 1,
+      sm: 1
+    }
   };
 
   const navigateTo = (year, romanSceneNumber) => {
@@ -113,84 +108,50 @@ const ThematicThreads = ({ currentBgColour }) => {
           'absolute'} ${currentBgColour === background ? 'show' : 'hide'}`}
       >
         {content.map((thread, index) => {
-          if (index % 2 === 0) {
-            return (
-              <span key={thread.title} className="contents">
-                <Col className="headline__col" {...evenTitleCols}>
-                  <div
-                    className={`thematic-thread-headline ${textColourClass} cursor-pointer pt-9 pb-5 ${fadeOrder[index]}`}
-                  >
-                    {ReactHtmlParser(thread.title)}
-                  </div>
-                </Col>
-                <Col className="grid__col" {...evenTitleGutterRightCols} />
-                <Col className="grid__col" {...evenSubtitleGutterLeftCols} />
-                <Col className="grid__col" {...evenSubtitleCols}>
-                  {thread.subtitles.map(subtitle => {
-                    return (
-                      <div
-                        key={subtitle}
-                        className={`small-headline ${textColourClass} cursor-pointer flex mb-1 ${fadeOrder[index]}`}
-                        onClick={() =>
-                          navigateTo(subtitle.year, subtitle.romanSceneNumber)
-                        }
-                      >
-                        {ReactHtmlParser(subtitle.year)}
-                        <div className="md:ml-16 sm:ml-8">
-                          {ReactHtmlParser(subtitle.title)}
-                        </div>
-                        <div className="small-body self-center lg:ml-64 md:ml-16 sm:ml-8">
-                          {ReactHtmlParser(
-                            `Scene&nbsp;${subtitle.romanSceneNumber}`
-                          )}
-                        </div>
+          const type = index % 2 === 0 ? 'even' : 'odd';
+          const cols = type === 'even' ? evenCols : oddCols;
+
+          return (
+            <span key={thread.title} className="contents">
+              {type === 'odd' && (
+                <Col className="grid__col" {...cols.titleGutterLeft} />
+              )}
+              <Col className="headline__col" {...cols.title}>
+                <div
+                  className={`thematic-thread-headline ${textColourClass} cursor-pointer pt-9 pb-5 ${fadeOrder[index]}`}
+                >
+                  {ReactHtmlParser(thread.title)}
+                </div>
+              </Col>
+              <Col className="grid__col" {...cols.titleGutterRight} />
+              <Col className="grid__col" {...cols.subtitleGutterLeft} />
+              <Col className="grid__col" {...cols.subtitle}>
+                {thread.subtitles.map(subtitle => {
+                  return (
+                    <div
+                      key={subtitle}
+                      className={`small-headline ${textColourClass} cursor-pointer flex mb-1 ${fadeOrder[index]}`}
+                      onClick={() =>
+                        navigateTo(subtitle.year, subtitle.romanSceneNumber)
+                      }
+                    >
+                      {ReactHtmlParser(subtitle.year)}
+                      <div className="md:ml-16 sm:ml-8">
+                        {ReactHtmlParser(subtitle.title)}
                       </div>
-                    );
-                  })}
-                </Col>
-                <Col className="grid__col" {...evenSubtitleGutterRightCols} />
-              </span>
-            );
-          } else {
-            return (
-              <span key={thread.title} className="contents">
-                <Col className="grid__col" {...oddTitleGutterLeftCols} />
-                <Col className="headline__col" {...oddTitleCols}>
-                  <div
-                    className={`thematic-thread-headline ${textColourClass} cursor-pointer pt-9 pb-5 ${fadeOrder[index]}`}
-                  >
-                    {ReactHtmlParser(thread.title)}
-                  </div>
-                </Col>
-                <Col className="grid__col" {...oddTitleGutterRightCols} />
-                <Col className="grid__col" {...oddSubtitleGutterLeftCols} />
-                <Col className="grid__col" {...oddSubtitleCols}>
-                  {thread.subtitles.map(subtitle => {
-                    return (
-                      <div
-                        key={subtitle}
-                        className={`small-headline ${textColourClass} cursor-pointer flex mb-1 ${fadeOrder[index]}`}
-                        onClick={() =>
-                          navigateTo(subtitle.year, subtitle.romanSceneNumber)
-                        }
-                      >
-                        {ReactHtmlParser(subtitle.year)}
-                        <div className="md:ml-16 sm:ml-8">
-                          {ReactHtmlParser(subtitle.title)}
-                        </div>
-                        <div className="small-body self-center lg:ml-64 md:ml-16 sm:ml-8">
-                          {ReactHtmlParser(
-                            `Scene&nbsp;${subtitle.romanSceneNumber}`
-                          )}
-                        </div>
+                      <div className="small-body self-center lg:ml-64 md:ml-16 sm:ml-8">
+                        {ReactHtmlParser(
+                          `Scene&nbsp;${subtitle.romanSceneNumber}`
+                        )}
                       </div>
-                    );
-                  })}
-                </Col>
-                <Col className="grid__col" {...oddSubtitleGutterRightCols} />
-              </span>
-            );
-          }
+                    </div>
+                  );
+                })}
+              </Col>
+              <Col className="grid__col" {...cols.subtitleGutterRight} />
+            </span>
+          );
+          // }
         })}
       </Row>
     );
