14fb7698f17d529a31899bbf586acf9d8b9ad9dc
Madeline Petersen <9222076+maddleline@users.noreply.github.com>
2022-12-18 15:08:30 -0800

    Make anecdote full width on small screens

--- src/components/Anecdote.jsx
+++ src/components/Anecdote.jsx
@@ -4,7 +4,7 @@ import { Close20 } from '@carbon/icons-react';
 import parse from 'html-react-parser';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Col, Container, Row, Visible } from 'react-grid-system';
+import { Col, Container, Hidden, Row, Visible } from 'react-grid-system';
 
 const Anecdote = ({
   type,
@@ -104,80 +104,96 @@ const Anecdote = ({
 
               {excerptTypes.includes(type) && (
                 <>
-                  <Col lg={2} md={2} sm={1} xs={1} onClick={onClickSpan} />
-                  <Col lg={1} md={2} sm={1} xs={1} />
-                  <Col lg={7} md={7} sm={9} xs={9} className="pt-8">
-                    <div className="bg-white z-30 pt-8 anecdote-fixed-header">
-                      <div className="small-body text-gray-90 mb-8">
-                        {shortTitle !== title ? parse(shortTitle) : ''}
-                      </div>
-                    </div>
-                  </Col>
-                  <Col lg={2} md={1} sm={1} xs={1} />
-
-                  <Col lg={2} md={2} sm={1} xs={1} onClick={onClickSpan} />
-                  <Col lg={1} md={2} sm={1} xs={1} />
-                  <Col lg={7} md={7} sm={9} xs={9} className="anecdote-content">
-                    <div className="large-headline-static mb-4">
-                      {parse(title)}
-                    </div>
-                    <div className="small-headline text-gray-90 md:mb-16 sm:mb-12">
-                      {parse(author)}
-                      {publication && `, ${publication}`}
-                      {year && `, ${year}`}
-                    </div>
-                  </Col>
-                  <Col lg={2} md={1} sm={1} xs={1} />
-
-                  <Col lg={2} md={2} sm={1} xs={1} onClick={onClickSpan} />
-                  <Col lg={2} md={2} sm={1} xs={1} />
-                  <Col lg={5} md={7} sm={9} xs={9}>
-                    <>
-                      {preamble && (
-                        <div className="small-body text-gray-90">
-                          {parse(preamble)}
-                        </div>
-                      )}
-                      {content &&
-                        content.map((paragraph, index) => {
-                          return (
-                            <div
-                              className="medium-body text-gray-90"
-                              key={`paragraph-${index}`}
-                            >
-                              {parse(paragraph)}
-                              <br />
-                            </div>
-                          );
-                        })}
-                      <div className="h-12" />
-                      <p className="border-t border-black border-opacity-10 pb-5" />
-                      {postamble && (
+                  <>
+                    <Hidden xs sm>
+                      <Col lg={2} md={2} sm={0} xs={0} onClick={onClickSpan} />
+                    </Hidden>
+                    <Col lg={1} md={2} sm={1} xs={1} />
+                    <Col lg={7} md={7} sm={10} xs={10} className="pt-8">
+                      <div className="bg-white z-30 pt-8 anecdote-fixed-header">
                         <div className="small-body text-gray-90 mb-8">
-                          {parse(postamble)}
+                          {shortTitle !== title ? parse(shortTitle) : ''}
                         </div>
-                      )}
-                      <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href={linkTo}
-                      >
-                        <p className="small-body text-black text-opacity-60 hover:text-opacity-100 transition-all">
-                          {parse(citation)}
-                        </p>
-                      </a>
-                      <div className="bottom-spacer" />
-                    </>
-                  </Col>
-                  <Col lg={3} md={1} sm={1} xs={1} />
+                      </div>
+                    </Col>
+                    <Col lg={2} md={1} sm={1} xs={1} />
+
+                    <Hidden xs sm>
+                      <Col lg={2} md={2} sm={0} xs={0} onClick={onClickSpan} />
+                    </Hidden>
+                    <Col lg={1} md={2} sm={1} xs={1} />
+                    <Col
+                      lg={7}
+                      md={7}
+                      sm={10}
+                      xs={10}
+                      className="anecdote-content"
+                    >
+                      <div className="large-headline-static mb-4">
+                        {parse(title)}
+                      </div>
+                      <div className="small-headline text-gray-90 md:mb-16 sm:mb-12">
+                        {parse(author)}
+                        {publication && `, ${publication}`}
+                        {year && `, ${year}`}
+                      </div>
+                    </Col>
+                    <Col lg={2} md={1} sm={1} xs={1} />
+
+                    <Hidden xs sm>
+                      <Col lg={2} md={2} sm={0} xs={0} onClick={onClickSpan} />
+                    </Hidden>
+                    <Col lg={2} md={2} sm={1} xs={1} />
+                    <Col lg={5} md={7} sm={10} xs={10}>
+                      <>
+                        {preamble && (
+                          <div className="small-body text-gray-90">
+                            {parse(preamble)}
+                          </div>
+                        )}
+                        {content &&
+                          content.map((paragraph, index) => {
+                            return (
+                              <div
+                                className="medium-body text-gray-90"
+                                key={`paragraph-${index}`}
+                              >
+                                {parse(paragraph)}
+                                <br />
+                              </div>
+                            );
+                          })}
+                        <div className="h-12" />
+                        <p className="border-t border-black border-opacity-10 pb-5" />
+                        {postamble && (
+                          <div className="small-body text-gray-90 mb-8">
+                            {parse(postamble)}
+                          </div>
+                        )}
+                        <a
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          href={linkTo}
+                        >
+                          <p className="small-body text-black text-opacity-60 hover:text-opacity-100 transition-all">
+                            {parse(citation)}
+                          </p>
+                        </a>
+                        <div className="bottom-spacer" />
+                      </>
+                    </Col>
+                    <Col lg={3} md={1} sm={1} xs={1} />
+                  </>
                 </>
               )}
 
               {type === 'Imagery' && (
                 <>
-                  <Col lg={4} md={2} sm={1} xs={1} onClick={onClickSpan} />
+                  <Hidden xs sm>
+                    <Col lg={4} md={2} sm={0} xs={0} onClick={onClickSpan} />
+                  </Hidden>
                   <Col lg={1} md={2} sm={1} xs={1} />
-                  <Col lg={6} md={7} sm={9} xs={9} className="pt-8">
+                  <Col lg={6} md={7} sm={10} xs={10} className="pt-8">
                     <div className="bg-white z-30 pt-8 anecdote-fixed-header">
                       <div className="small-body text-gray-90 mb-8">
                         {shortTitle !== title ? parse(shortTitle) : ''}
@@ -186,9 +202,17 @@ const Anecdote = ({
                   </Col>
                   <Col lg={1} md={1} sm={1} xs={1} />
 
-                  <Col lg={4} md={2} sm={1} xs={1} onClick={onClickSpan} />
+                  <Hidden xs sm>
+                    <Col lg={4} md={2} sm={1} xs={1} onClick={onClickSpan} />
+                  </Hidden>
                   <Col lg={1} md={2} sm={1} xs={1} />
-                  <Col lg={6} md={7} sm={9} xs={9} className="anecdote-content">
+                  <Col
+                    lg={6}
+                    md={7}
+                    sm={10}
+                    xs={10}
+                    className="anecdote-content"
+                  >
                     <div className="large-headline-static mb-4">
                       {parse(title)}
                     </div>
@@ -205,9 +229,11 @@ const Anecdote = ({
                   </Col>
                   <Col lg={1} md={1} sm={1} xs={1} />
 
-                  <Col lg={4} md={2} sm={1} xs={1} onClick={onClickSpan} />
+                  <Hidden xs sm>
+                    <Col lg={4} md={2} sm={0} xs={0} onClick={onClickSpan} />
+                  </Hidden>
                   <Col lg={1} md={2} sm={1} xs={1} />
-                  <Col lg={7} md={7} sm={9} xs={9}>
+                  <Col lg={7} md={7} sm={10} xs={10}>
                     {content &&
                       content.images.map((image, index) => {
                         return (
@@ -241,9 +267,11 @@ const Anecdote = ({
 
               {poemTypes.includes(type) && (
                 <>
-                  <Col lg={6} md={2} sm={1} xs={1} onClick={onClickSpan} />
+                  <Hidden xs sm>
+                    <Col lg={6} md={2} sm={0} xs={0} onClick={onClickSpan} />
+                  </Hidden>
                   <Col lg={1} md={2} sm={1} xs={1} />
-                  <Col lg={5} md={7} sm={9} xs={9}>
+                  <Col lg={5} md={7} sm={10} xs={10}>
                     <div className="bg-white z-30 pt-8 anecdote-fixed-header">
                       <div className="small-body text-gray-90 mb-8">
                         {shortTitle !== title ? parse(shortTitle) : ''}
@@ -254,9 +282,17 @@ const Anecdote = ({
                     <Col md={1} sm={1} xs={1} />
                   </Visible>
 
-                  <Col lg={6} md={2} sm={1} xs={1} onClick={onClickSpan} />
+                  <Hidden xs sm>
+                    <Col lg={6} md={2} sm={0} xs={0} onClick={onClickSpan} />
+                  </Hidden>
                   <Col lg={1} md={2} sm={1} xs={1} />
-                  <Col lg={5} md={7} sm={9} xs={9} className="anecdote-content">
+                  <Col
+                    lg={5}
+                    md={7}
+                    sm={10}
+                    xs={10}
+                    className="anecdote-content"
+                  >
                     <div className="large-headline-static mb-4">
                       {parse(title)}
                     </div>
